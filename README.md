5df7d003c263a4ccad121ff22c3b573b64a7f3c6
andreas.martin01 <andreas.martin01@funkemedien.de>
2024-07-09 14:31:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Pimcore List Widget
+
+<!-- TOC -->
+* [Pimcore Widget](#pimcore-list-widget)
+  * [Requirements](#requirements)
+  * [Installation](#installation)
+  * [Good to know](#good-to-know)
+  * [Further Information](#further-information)
+<!-- TOC -->
+
+### Requirements
+This widget is designed for use with Pimcore 10.2 or higher.
+
+### Installation
+Install the widget using composer:
+
+```shell
+composer require werstreamtes/pimcore-list-widget
+```
+
+or via composer.json:
+```json
+"require" : {
+    "werstreamtes/pimcore-list-widget" : "1.0.0",
+}
+```
+
+Activate the widget using the following command:
+```shell
+bin/console pimcore:bundle:enable WerStreamtEsListWidgetBundle
+```
+
+### Good to know
+
+If you are part of the WerStreamt.es partner program, you can save your tag in the website settings.
+
+Go to the website settings and create a new entry with the key `wseWidgetTag` of type `Text`. You can fill this with your partner tag.
+
+![General Settings](settings.gif)
+
+### Further Information
+
+* [WerStreamt.es Developer Widget Documentation](https://www.werstreamt.es/developers/widget/)
+
+
+Feel free to reach out if you have any questions or issues!
---
+++ src/Document/Areabrick/ListWidget.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Pimcore\Bundle\WidgetBundle\Document\Areabrick;
+
+use Pimcore\Extension\Document\Areabrick\AbstractTemplateAreabrick;
+use Pimcore\Extension\Document\Areabrick\EditableDialogBoxConfiguration;
+use Pimcore\Extension\Document\Areabrick\EditableDialogBoxInterface;
+use Pimcore\Model\Document\Editable\Area\Info;
+
+class ListWidget extends AbstractTemplateAreabrick implements EditableDialogBoxInterface
+{
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'WerStreamt.es List Widget';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return 'Mit dem Widget kannst du Verfügbarkeitsinformationen zu Filmen und Serien direkt auf deiner Webseite anzeigen.';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTemplateLocation()
+    {
+        return static::TEMPLATE_LOCATION_BUNDLE;
+    }
+
+    /**
+     * @param $area
+     * @param $info
+     * @return EditableDialogBoxConfiguration
+     */
+    public function getEditableDialogBoxConfiguration($area, $info): EditableDialogBoxConfiguration
+    {
+
+        $config = new EditableDialogBoxConfiguration();
+        $config->setReloadOnClose(true);
+
+        $lifetime = (3600 * 16);
+        $cacheKey = 'providers';
+        $cachData = \Pimcore\Cache::load($cacheKey);
+        if (!$cachData) {
+
+            $providers = @file_get_contents('https://www.werstreamt.es/widgets/filter/providers/');
+
+            if ($providers) {
+                $providers = @json_decode($providers, true);
+                if (is_array($providers)) {
+                    $providers = array_map(function ($provider) {
+                        return [$provider['alias'], $provider['title']];
+                    }, $providers);
+                }
+            }
+
+            \Pimcore\Cache::save($providers, $cacheKey, ['providers'], $lifetime);
+        } else {
+            $providers = $cachData;
+        }
+
+        $config->setItems([
+            [
+                'type' => 'input',
+                'label' => 'URL zur Liste',
+                'name' => 'url'
+            ],
+            [
+                'type' => 'select',
+                'name' => 'layout',
+                'label' => 'Darstellung wählen',
+                'config' => [
+                    'store' => [
+                        [1, 'Einspaltig'],
+                        [2, 'Zweispaltig']
+                    ],
+                    'defaultValue' => 1
+                ]
+            ],
+            [
+                'type' => 'numeric',
+                'name' => 'limit',
+                'label' => 'Wie viele Einträge sollen angezeigt werden?',
+                'config' => [
+                    'minValue' => 1,
+                    'maxValue' => 20
+                ]
+            ],
+            [
+                'type' => 'checkbox',
+                'name' => 'showNumbers',
+                'label' => 'Nummerierung anzeigen?'
+            ],
+            [
+                'type' => 'multiselect',
+                'name' => 'providers',
+                'label' => 'Anbieter auswählen',
+                'config' => [
+                    'store' => is_array($providers) ? $providers : []
+                ]
+            ]
+        ]);
+
+        return $config;
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function needsReload(): bool
+    {
+        return true;
+    }
+}
